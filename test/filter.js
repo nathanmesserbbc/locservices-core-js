c1d918d7a391ab04c0b79b8397a054ea59a876d0
James Cryer <info@jamescryer.com>
2014-07-10 18:01:09 +0100

    MYLOC-107 - Adding tests

--- test/filter.js
+++ test/filter.js
@@ -3,6 +3,46 @@ module("Filter", {
   }
 });
 
-test("boilerplate", function() {
-  ok(true);
+test("should filter locations by placeType", function() {
+  ok(false);
+});
+
+test("should filter locations by country", function() {
+  ok(false);
+});
+
+test("should filter locations by placeType and country", function() {
+  ok(false);
+});
+
+test("should filter locations by domestic filter", function() {
+  ok(false);
+});
+
+test("should filter locations by domestic filter, placeType", function() {
+  ok(false);
+});
+
+test("should filter locations by domestic filter, countries", function() {
+  ok(false);
+});
+
+test("should filter locations by domestic filter, placeType and countries", function() {
+  ok(false);
+});
+
+test("should filter locations by international filter", function() {
+  ok(false);
+});
+
+test("should filter locations by international filter, placeType", function() {
+  ok(false);
+});
+
+test("should filter locations by international filter, countries", function() {
+  ok(false);
+});
+
+test("should filter locations by international filter, placeType and countries", function() {
+  ok(false);
 });
